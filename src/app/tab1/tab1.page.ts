ca5927faeac386fc931be4b0e2035dd800651f95
Pedro Agriarte <agriarte@hotmail.com>
2021-06-23 00:11:23 +0200

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+//desde docu oficial Capacitor buscar para instalar Geolocation.
+//npm audit npm install @capacitor/geolocation
+//Mucha atención. Existe Geolocation Nativo y de Capacitor. Usar uno o otro tiene
+//diferencias y saldrán errores en el código
+
+
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +14,23 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  //parecido a getElementbyId. En la vista HTML se refiere a #map no al id.
+  @ViewChild('map', { static: false }) mapElement: ElementRef;
+
+  posicion: string;//para interpolar en la vista
+
+  //como Geolocation es estático no hace falta declararlo en el constructor
+  //ejemplos docu oficial, en cambio, lo hace.
+  constructor() { }
+
+  ionViewDidEnter() {
+    this.getLocation();
+  }
+
+  async getLocation() {
+    const coordenates = await Geolocation.getCurrentPosition();
+    this.posicion = 'Latitud: ' + coordenates.coords.latitude + ' Longitud: ' + coordenates.coords.longitude
+    console.log(this.posicion);
+  }
 
 }
